ba4563ebf9c5e08080666edb87e64ffd2f765b24
Ivan Escobar <ivnescbr1989@gmail.com>
2025-02-07 18:19:29 -0500

    added reqs to read me || logout btn && logout logic

--- components/logout-btn.tsx
+++ components/logout-btn.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useAuthStore } from '@/store/authStore'
+import { useAuthStore } from '../store/auth-store'
 import { useRouter } from 'next/navigation'
 
 export default function LogoutButton() {
--- store/auth-store.ts
+++ store/auth-store.ts
@@ -1,11 +1,21 @@
+import axios from 'axios'
 import { create } from 'zustand'
 
 interface AuthState {
   isAuthenticated: boolean
   setAuthenticated: (auth: boolean) => void
+  logout: () => void
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
   setAuthenticated: (auth) => set({ isAuthenticated: auth }),
+  logout: async () => {
+    await axios.post(
+      'https://frontend-take-home-service.fetch.com/auth/logout',
+      {},
+      { withCredentials: true }
+    )
+    set({ isAuthenticated: false })
+  },
 }))
